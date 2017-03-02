59faed61bd825a789f1da20d370eaf7f6c9425da
桝田研仁 <akihito@masudakenhitoshinoMacBook-Pro.local>
2017-03-03 00:51:59 +0900

    進むボタンをif文で書いて見る

--- slideshow/ViewController.swift
+++ slideshow/ViewController.swift
@@ -16,18 +16,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var susumu: UIButton!
     @IBOutlet weak var modoru: UIButton!
     
-    //時間送りのための変数を定義（型はタイマー）
+    //再生ボタンタップしたとき時間送りのための変数を定義（型はタイマー）
     var timer: Timer!
     
     //画像名の配列を設定
     let animal = ["bear.jpg","cat.jpg","doga.jpg","pig.jpg"]
     //配列番号を指定するためのナンバーを定義
     var num: Int = 0
+    //画像の最大枚数を定義
+    var maxanimal: Int = 4
+    //画像の最低枚数を定義
+    var minanimal: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
+        //起動時に画像を表示させておくんです
+        ImageView.image = UIImage(named: animal[0])
+        
     }
 
     //試しに画像をUIImageに表示させてみる
@@ -35,25 +42,33 @@ class ViewController: UIViewController {
     //再生ボタンをアクション接続
     @IBAction func Saisei(_ sender: Any) {
         
-
-        
     }
     
     //進むボタンをアクション接続
-    @IBAction func susumu(_ sender: Any) {
-
-        //numに１ずつプラスしていく
-        num += 1
-        //進むボタンをタップしたとき、画像を出力
-        //出力する画像は、以下
-        ImageView.image = UIImage(named: animal[num])
-
-        
+    @IBAction func Susumu(_ sender: Any) {
+        //もし、numがmaxanimalから1を引いた数よりちっこかったら
+        if num < maxanimal-1{
+        //numに１ずつ足していく
+            num += 1
+            //画像を表示する
+            ImageView.image = UIImage(named: animal[num])
+            
+        }
     }
     
     //戻るボタンをアクション接続
     @IBAction func Modoru(_ sender: Any) {
+        
+        //numに１ずつマイナスしていく
+        num -= 1
+        //進むボタンをタップしたとき、画像を出力
+        //出力する画像は、以下
+        ImageView.image = UIImage(named: animal[num])
+        
     }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
