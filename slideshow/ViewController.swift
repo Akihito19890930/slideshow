84034f1244f3c8611235073cc179dff8ce755684
桝田研仁 <akihito@masudakenhitoshinoMacBook-Pro.local>
2017-03-03 00:08:01 +0900

    進むボタンのメソッドを作って見るも、シミュレーターでエラー！

--- slideshow/ViewController.swift
+++ slideshow/ViewController.swift
@@ -16,32 +16,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var susumu: UIButton!
     @IBOutlet weak var modoru: UIButton!
     
+    //時間送りのための変数を定義（型はタイマー）
+    var timer: Timer!
+    
     //画像名の配列を設定
     let animal = ["bear.jpg","cat.jpg","doga.jpg","pig.jpg"]
+    //配列番号を指定するためのナンバーを定義
+    var num: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    
-        
-        //UIImageのインスタンスを生成。
-        //namedが作るときに渡してあげる情報。
-        //UIImageViewで定義されている、imageというプロパティ
-        ImageView.image = UIImage(named: animal[0])
 
-        
     }
 
     //試しに画像をUIImageに表示させてみる
 
     //再生ボタンをアクション接続
     @IBAction func Saisei(_ sender: Any) {
-    
+        
+
         
     }
     
     //進むボタンをアクション接続
-    @IBAction func Susumu(_ sender: Any) {
+    @IBAction func susumu(_ sender: Any) {
+
+        //numに１ずつプラスしていく
+        num += 1
+        //進むボタンをタップしたとき、画像を出力
+        //出力する画像は、以下
+        ImageView.image = UIImage(named: animal[num])
+
+        
     }
     
     //戻るボタンをアクション接続
