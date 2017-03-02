09ee0448e755a5d9d848192a0dfb5d1694cb9ab8
桝田研仁 <akihito@masudakenhitoshinoMacBook-Pro.local>
2017-03-02 23:38:55 +0900

    0302

--- slideshow/ViewController.swift
+++ slideshow/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //ImageViewをアウトレット接続
+    @IBOutlet weak var ImageView: UIImageView!
+    @IBOutlet weak var saisei: UIButton!
+    @IBOutlet weak var susumu: UIButton!
+    @IBOutlet weak var modoru: UIButton!
+    
+    //画像名の配列を設定
+    let animal = ["bear.jpg","cat.jpg","doga.jpg","pig.jpg"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        
+        //UIImageのインスタンスを生成。
+        //namedが作るときに渡してあげる情報。
+        //UIImageViewで定義されている、imageというプロパティ
+        ImageView.image = UIImage(named: animal[0])
+
+        
     }
 
+    //試しに画像をUIImageに表示させてみる
+
+    //再生ボタンをアクション接続
+    @IBAction func Saisei(_ sender: Any) {
+    
+        
+    }
+    
+    //進むボタンをアクション接続
+    @IBAction func Susumu(_ sender: Any) {
+    }
+    
+    //戻るボタンをアクション接続
+    @IBAction func Modoru(_ sender: Any) {
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
